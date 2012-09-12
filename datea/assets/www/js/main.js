f20243b92c87a0c5be052b3df9efb7feb7499a94
decolecor <cmart@decolecor.net>
2012-09-12 02:31:32 -0300

    working on localsession and home issue

--- datea/assets/www/js/main.js
+++ datea/assets/www/js/main.js
@@ -1,21 +1,35 @@
 var api_url = "http://192.168.2.113:8000";
 
 var DateaRouter = Backbone.Router.extend({
+     
 	routes: {
 		"": "home",
 		"login": "login",
 		"about": "about",
 		"user/:userid": "userLoadProfile",
-        "user/edit/:userid": "userEditProfile",
-        "actions": "allActions"
+                "user/edit/:userid": "userEditProfile",
+                "actions": "allActions"
 	},
 	
 	initialize: function () {
 		$.ajaxSetup({ crossDomain:true });
 		$.support.cors = true;
 		
-		this.headerView = new HeaderView();
-		$('.header').html(this.headerView.render().el);
+                if(local_session.get('logged')){
+                    this.headerView = new HeaderView();
+                    $('.header').html(this.headerView.render().el);
+                    var userid = local_session.get('userid');
+                    localUser.fetch({ data: { 'id': userid }});
+                    this.profileView = new ProfileView({ model: localUser });
+                    $("#app").html(this.profileView.render().el);
+
+                }else{
+                    if(!this.homeView){
+                        this.homeView = new HomeView();
+                    }
+                $('#app').html(this.homeView.render().el);
+
+                }
 	},
 
 	home: function(){
@@ -47,38 +61,45 @@ var DateaRouter = Backbone.Router.extend({
 	},
 	
 	userLoadProfile: function (userid) {
-		localUser.fetch({ data: { 'id': userid }});
-        this.profileView = new ProfileView({ model: localUser });
-        $("#app").html(this.profileView.render().el);
+            localUser.fetch({ data: { 'id': userid }});
+            this.profileView = new ProfileView({ model: localUser });
+            $("#app").html(this.profileView.render().el);
 	},
 	
 	userEditProfile: function (userid) {
 		this.profileEditView = new ProfileEditView({ model: localUser });
-        $("#app").html(this.profileEditView.render().el);
+                $("#app").html(this.profileEditView.render().el);
 	},
 	
 	allActions: function () {
-		alert('entro a allActions')
+		alert('entro a allActions');
 		this.actionCollection = new ActionCollection();
-        var self = this;
-        this.actionCollection.fetch({
-            success: function(collection, response) {
+                var self = this;
+                this.actionCollection.fetch({
+                success: function(collection, response) {
                 $("#app").html(new ActionsView({ model: self.actionCollection }).render().el);
             }
         }); 
 	}
 });
 
-utils.loadTpl(['HeaderView', 'AboutView', 'LoginView', 'ProfileView',
-               'ProfileEditView', 'HomeView', 'ActionsView'], function () {
+$(document).ready(function(){
+    utils.loadTpl(['HeaderView', 'AboutView', 'LoginView', 'ProfileView',
+                   'ProfileEditView', 'HomeView', 'ActionsView'], function () {
 
-	Backbone.Tastypie.prependDomain = api_url || "http://10.0.2.2:8000";
-	
-	window.localSession = new localSession();
-	console.log(window.localSession);
-    window.localUser = new User();
-	
-	var app = window.app = new DateaRouter();
-	
-	Backbone.history.start();
+            Backbone.Tastypie.prependDomain = api_url || "http://10.0.2.2:8000";
+            
+            window.local_session = new localSession();
+            //console.log(localSession);
+
+            window.localUser = new User();
+
+            if(localStorage.getItem('authdata') !== undefined){
+                var authdata = JSON.parse(localStorage.getItem('authdata'));
+                local_session.set(authdata);
+            }
+            console.log("init router");
+            window.dateaApp = new DateaRouter();           
+            Backbone.history.start();
+    });
 });
