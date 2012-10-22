3d506e6472a44a1af2c84af9e7a0aa650e0cdebb
decolecor <cmart@decolecor.net>
2012-10-22 19:54:56 -0300

    search form and results, TODO testing

--- datea/assets/www/js/views/search_form.js
+++ datea/assets/www/js/views/search_form.js
@@ -15,9 +15,10 @@ var SearchFormView = Backbone.View.extend({
         'change #order_by': 'set_order',
         'click #submit_search': 'search'
     },
-
     render: function(){
-        this.$el.html(this.template());
+        var context = { 'cat_list': this.model.toJSON()};
+        this.$el.html(this.template(context));
+
         return this;
     },
 
@@ -26,22 +27,30 @@ var SearchFormView = Backbone.View.extend({
         console.log("search term: " + this.search_term); 
     },
 
-    set_category: function(){
-        console.log("set category");
-
+    set_category: function(event){
+        this.category = event.currentTarget.value;
+        console.log("set category: " + this.category);
     },
 
     set_order: function(event){
         //this.params.order_by = $('#order_by').val();
-        this.order = event.currentTarget.value
+        this.order = event.currentTarget.value;
         console.log("setting order: " + this.order); 
     },
     search:function(event){
         event.preventDefault();
         console.log("submit search: " + this.search_term + this.category + this.order_by );
+        
         dateaApp.navigate("/search/" + this.search_term + "/" + this.category + "/" + this.order ,
                 {trigger: true}
                 );
+    
+        /*
+        dateaApp.navigate("/search/" + "perros" + "/" + "1" + "/" + 'distance' ,
+                {trigger: true}
+                );
+        */
+
         /*
         var params = {
             limit : this.items_per_page,
